bc30d0307d79d5971f7d0a807ba309f10876fbf5
= <mygurmi@naver.com>
2015-01-18 16:12:01 +0900

    codink

---
+++ codink/codink.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+	$('.panel panel-default').mouseover(function(){
+		$('.panel panel-default').addClass('jiyoon2');
+	});
+
+	$('.panel-heading').mouseout(function(){
+		$('.panel panel-default').removeClass('jiyoon2');		 
+	});	
+
+
+});
+
+
+
---
+++ codink/taptest.js
@@ -0,0 +1,8 @@
+$('#myCollapsible').collapse('toggle'{
+  toggle: false
+})
+
+
+$('#myCollapsible').on('hidden', function () {
+  // 무언가를 한다…
+})
